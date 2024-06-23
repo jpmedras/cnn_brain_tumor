78e06896f11f87814ba243131a34349af968d73b
João Pedro Sena <joaopedromsena@gmail.com>
2024-06-22 22:04:01 -0300

    Adicionando versão do modelo

--- src/model.py
+++ src/model.py
@@ -1,12 +1,13 @@
 import numpy as np
 from torch import manual_seed
 import random
-from torchvision.transforms import Compose, ToTensor, Grayscale
+from torchvision.transforms import ToTensor
 from torch import device
 from torch import cuda
 from torchvision.datasets import ImageFolder
 from torch.utils.data import random_split
-from torch.utils.data import DataLoader
+from torch.utils.data import ConcatDataset, DataLoader
+from torch.nn import Module
 from torchvision.models import alexnet, AlexNet_Weights
 from torch.nn import Conv2d, Linear, BCELoss
 from torch.optim import SGD
@@ -31,32 +32,34 @@ def plot_img(image, label=None):
         plt.title(f'Image in Tensor format | Class: {label:2d}')
     plt.show()   
 
-class AlexNet:
+class AlexNet(Module):
     if cuda.is_available():
         _DEVICE = device('cuda')
     else:
         _DEVICE = device('cpu')
 
-    def __init__(self, debug=False) -> None:        
+    def __init__(self, debug=False) -> None:
+        super().__init__()
+
         self.model = alexnet(weights=AlexNet_Weights.DEFAULT)
 
         for param in self.model.parameters():
             param.requires_grad = False
+       
+        self.model.classifier[6] = Linear(4096,2)
+
+        self.model.classifier[6].requires_grad = True
 
         if debug:
             for name, module in self.model.named_modules():
                 print(name, module)
-        
-        # self.model.features[0] = Conv2d(1, 64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2))
-        self.model.classifier[6] = Linear(4096,2)
-
-        # self.model.classifier[0].requires_grad = True
-        # self.model.classifier[3].requires_grad = True
-        self.model.classifier[6].requires_grad = True
 
         self.model.to(self._DEVICE)
 
-    def fit(self, train_loader, test_loader=None, epochs=10, lr=1e-3, debug=False):
+    def foward(self, x):
+        return self.model(x)
+
+    def fit(self, train_loader, test_loader=None, epochs=10, lr=1e-3, file_path='src/best_model.pt', debug=False):
         optimizer = SGD(self.model.parameters(), lr=lr)
         criterion = BCELoss()
 
@@ -65,7 +68,7 @@ class AlexNet:
         
         for epoch in tqdm(range(epochs), desc='Training epochs'):
             self.model.train()
-            for idx, (inputs, labels) in enumerate(train_loader):
+            for inputs, labels in train_loader:
                 inputs = inputs.to(self._DEVICE, dtype=float)
                 labels = labels.to(self._DEVICE, dtype=float)
 
@@ -87,12 +90,13 @@ class AlexNet:
 
                 if accuracy > max_acc:
                     max_acc = accuracy
-                    save(self.model.state_dict(), 'src/best_model.pt')
+                    save(self.model.state_dict(), file_path)
                     print(f"Saving new model in epoch {epoch} with new best accuracy in test:", accuracy)
 
-            # if debug:
-            #     plt.plot(accuracies)
-            #     plt.show()
+        if debug:
+            plt.title('Accuracy in train')
+            plt.plot(accuracies)
+            plt.show()
 
         return self.model
     
@@ -111,27 +115,19 @@ class AlexNet:
 
             total_len += len(inputs)
             corrects += (preds == labels).sum().item()
-
-            # plot_img(image=inputs[0].to('cpu'))
         
         return 100 * (corrects/total_len)
-    
-_transform = Compose([
-    ToTensor(),
-    # Grayscale(),
-])
 
-
-dataset = ImageFolder(root=data_root, transform=_transform)
+dataset = ImageFolder(root=data_root, transform=ToTensor())
 
 train_dataset, test_dataset = random_split(dataset, [0.8, 0.2])
 
 train_loader = DataLoader(train_dataset,
-                        batch_size=8,
+                        batch_size=32,
                         shuffle=True)
 test_loader = DataLoader(test_dataset,
-                        batch_size=8,
+                        batch_size=32,
                         shuffle=False)
 
 model = AlexNet(debug=False)
-model.fit(train_loader=train_loader, test_loader=test_loader, epochs=20, lr=1e-3, debug=True)
+model.fit(train_loader=train_loader, test_loader=test_loader, epochs=35, lr=1e-3, debug=True)
